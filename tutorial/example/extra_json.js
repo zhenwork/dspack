b64fecc92ce19a4853e46c50763a9601ecdd00d1
zhenwork <36317218+zhenwork@users.noreply.github.com>
2021-01-28 04:28:40 -0800

    Create extra_json.js

---
+++ tutorial/example/extra_json.js
@@ -0,0 +1,8 @@
+{
+    "thickness_m": 0.00032, 
+    "absorption_coefficient_invmm": 1.9952756842918657, 
+    "thickness_um": 320.0, 
+    "absorption_coefficient_invum": 0.0019952756842918657, 
+    "thickness_mm": 0.32,
+    "polarization_fr": -1
+}
